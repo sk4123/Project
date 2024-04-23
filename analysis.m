305c2744f0c7ee007835abe95b5e04a75c2830e1
sk4123 <66120293+sk4123@users.noreply.github.com>
2024-04-23 10:02:28 -0400

    4/23 - Random Uncertainty
    Finished the random uncertainty. Started adding values for systematic error from the data sheet.

--- analysis.m
+++ analysis.m
@@ -30,6 +30,21 @@ cc = 2*sqrt((m_cyl+m_plate)*k);
 
 c = zeta*cc;
 
+%% Systematic Uncertainty
+
+% Values from the optical sensor's datasheet
+% analog output from 4 mA to 20 mA?
+% assuming fast. what is it really?
+% need to calibrate the sensor for a) distance and b) zeta
+resd = 1; % mm
+repeatability = 5; % mm
+accuracy = 10; % mm
+responsetime = 20; % ms
+outputrate = 4; % ms
+resb = 16; % bits
+hyst = 10; % mm. Sheet says 10 ... 1000
+
+
 %% 10
 
 % Getting the fits
@@ -52,6 +67,8 @@ r10_d3 = fit(dr10_d3,a);
 
 vals = [r10_i1 r10_i2 r10_i3 r10_d1 r10_d2 r10_d3];
 
+old_eles = [dr10_i1(2,:)',dr10_i2(2,:)',dr10_i3(2,:)',dr10_d1(2,:)',dr10_d2(2,:)',dr10_d3(2,:)'];
+
 graph(dr10_i1,r10_i1,1);
 graph(dr10_i2,r10_i2,2);
 graph(dr10_i3,r10_i3,3);
@@ -59,10 +76,14 @@ graph(dr10_d1,r10_d1,4);
 graph(dr10_d2,r10_d2,5);
 graph(dr10_d3,r10_d3,6);
 
-% dr10 = fit([dr10_i1,dr10_i2,dr10_i3,dr10_d1,dr10_d2,dr10_d3],a);
-% graph([dr10_i1,dr10_i2,dr10_i3,dr10_d1,dr10_d2,dr10_d3],dr10,7)
-
 r10_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles); % V or mV?
 
 %% 15
 
@@ -86,6 +107,8 @@ r15_d3 = fit(r15_d3,a);
 
 vals = [r15_i1 r15_i2 r15_i3 r15_d1 r15_d2 r15_d3];
 
+old_eles = [dr15_i1(2,:)',dr15_i2(2,:)',dr15_i3(2,:)',dr15_d1(2,:)',dr15_d2(2,:)',dr15_d3(2,:)'];
+
 graph(dr15_i1,r15_i1,1);
 graph(dr15_i2,r15_i2,2);
 graph(dr15_i3,r15_i3,3);
@@ -94,6 +117,14 @@ graph(dr15_d2,r15_d2,5);
 graph(dr15_d3,r15_d3,6);
 
 r15_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
+
 
 %% 20
 
@@ -117,6 +148,8 @@ r20_d3 = fit(r20_d3,a);
 
 vals = [r20_i1 r20_i2 r20_i3 r20_d1 r20_d2 r20_d3];
 
+old_eles = [dr20_i1(2,:)',dr20_i2(2,:)',dr20_i3(2,:)',dr20_d1(2,:)',dr20_d2(2,:)',dr20_d3(2,:)'];
+
 graph(dr20_i1,r20_i1,1);
 graph(dr20_i2,r20_i2,2);
 graph(dr20_i3,r20_i3,3);
@@ -125,6 +158,13 @@ graph(dr20_d2,r20_d2,5);
 graph(dr20_d3,r20_d3,6);
 
 r20_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 25
 
@@ -148,6 +188,8 @@ r25_d3 = fit(dr25_d3,a);
 
 vals = [r25_i1 r25_i2 r25_i3 r25_d1 r25_d2 r25_d3];
 
+old_eles = [dr25_i1(2,:)',dr25_i2(2,:)',dr25_i3(2,:)',dr25_d1(2,:)',dr25_d2(2,:)',dr25_d3(2,:)'];
+
 graph(dr25_i1,r25_i1,1);
 graph(dr25_i2,r25_i2,2);
 graph(dr25_i3,r25_i3,3);
@@ -156,6 +198,13 @@ graph(dr25_d2,r25_d2,5);
 graph(dr25_d3,r25_d3,6);
 
 r25_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 30
 
@@ -179,6 +228,8 @@ r30_d3 = fit(dr30_d3,[0.5 15 -1 1]);
 
 vals = [r30_i1 r30_i2 r30_i3 r30_d1 r30_d2 r30_d3];
 
+old_eles = [dr30_i1(2,:)',dr30_i2(2,:)',dr30_i3(2,:)',dr30_d1(2,:)',dr30_d2(2,:)',dr30_d3(2,:)'];
+
 graph(dr30_i1,r30_i1,1);
 graph(dr30_i2,r30_i2,2);
 graph(dr30_i3,r30_i3,3);
@@ -187,6 +238,13 @@ graph(dr30_d2,r30_d2,5);
 graph(dr30_d3,r30_d3,6);
 
 r30_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 35
 
@@ -210,6 +268,8 @@ r35_d3 = fit(dr35_d3,a);
 
 vals = [r35_i1 r35_i2 r35_i3 r35_d1 r35_d2 r35_d3];
 
+old_eles = [dr35_i1(2,:)',dr35_i2(2,:)',dr35_i3(2,:)',dr35_d1(2,:)',dr35_d2(2,:)',dr35_d3(2,:)'];
+
 graph(dr35_i1,r35_i1,1);
 graph(dr35_i2,r35_i2,2);
 graph(dr35_i3,r35_i3,3);
@@ -218,6 +278,13 @@ graph(dr35_d2,r35_d2,5);
 graph(dr35_d3,r35_d3,6);
 
 r35_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 40
 
@@ -241,6 +308,8 @@ r40_d3 = fit(dr40_d3,a);
 
 vals = [r40_i1 r40_i2 r40_i3 r40_d1 r40_d2 r40_d3];
 
+old_eles = [dr40_i1(2,:)',dr40_i2(2,:)',dr40_i3(2,:)',dr40_d1(2,:)',dr40_d2(2,:)',dr40_d3(2,:)'];
+
 graph(dr40_i1,r40_i1,1);
 graph(dr40_i2,r40_i2,2);
 graph(dr40_i3,r40_i3,3);
@@ -249,6 +318,13 @@ graph(dr40_d2,r40_d2,5);
 graph(dr40_d3,r40_d3,6);
 
 r40_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 45
 
@@ -272,6 +348,8 @@ r45_d3 = fit(dr45_d3,a);
 
 vals = [r45_i1 r45_i2 r45_i3 r45_d1 r45_d2 r45_d3];
 
+old_eles = [dr45_i1(2,:)',dr45_i2(2,:)',dr45_i3(2,:)',dr45_d1(2,:)',dr45_d2(2,:)',dr45_d3(2,:)'];
+
 graph(dr45_i1,r45_i1,1);
 graph(dr45_i2,r45_i2,2);
 graph(dr45_i3,r45_i3,3);
@@ -280,6 +358,13 @@ graph(dr45_d2,r45_d2,5);
 graph(dr45_d3,r45_d3,6);
 
 r45_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 50
 
@@ -303,6 +388,8 @@ r50_d3 = fit(dr50_d3,a);
 
 vals = [r50_i1 r50_i2 r50_i3 r50_d1 r50_d2 r50_d3];
 
+old_eles = [dr50_i1(2,:)',dr50_i2(2,:)',dr50_i3(2,:)',dr50_d1(2,:)',dr50_d2(2,:)',dr50_d3(2,:)'];
+
 graph(dr50_i1,r50_i1,1);
 graph(dr50_i2,r50_i2,2);
 graph(dr50_i3,r50_i3,3);
@@ -311,6 +398,13 @@ graph(dr50_d2,r50_d2,5);
 graph(dr50_d3,r50_d3,6);
 
 r50_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 55
 
@@ -334,6 +428,8 @@ r55_d3 = fit(dr55_d3,[0.04 33 0 1]);
 
 vals = [r55_i1 r55_i2 r55_i3 r55_d1 r55_d2 r55_d3];
 
+old_eles = [dr55_i1(2,:)',dr55_i2(2,:)',dr55_i3(2,:)',dr55_d1(2,:)',dr55_d2(2,:)',dr55_d3(2,:)'];
+
 graph(dr55_i1,r55_i1,1);
 graph(dr55_i2,r55_i2,2);
 graph(dr55_i3,r55_i3,3);
@@ -342,6 +438,13 @@ graph(dr55_d2,r55_d2,5);
 graph(dr55_d3,r55_d3,6);
 
 r55_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 60
 
@@ -365,6 +468,8 @@ r60_d3 = fit(dr60_d3,[0.027 35 0 1.1]);
 
 vals = [r60_i1 r60_i2 r60_i3 r60_d1 r60_d2 r60_d3];
 
+old_eles = [dr60_i1(2,:)',dr60_i2(2,:)',dr60_i3(2,:)',dr60_d1(2,:)',dr60_d2(2,:)',dr60_d3(2,:)'];
+
 graph(dr60_i1,r60_i1,1);
 graph(dr60_i2,r60_i2,2);
 graph(dr60_i3,r60_i3,3);
@@ -373,6 +478,13 @@ graph(dr60_d2,r60_d2,5);
 graph(dr60_d3,r60_d3,6);
 
 r60_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 65
 
@@ -396,6 +508,8 @@ r65_d3 = fit(dr65_d3,[0.027 35 0 1.1]);
 
 vals = [r65_i1 r65_i2 r65_i3 r65_d1 r65_d2 r65_d3];
 
+old_eles = [dr65_i1(2,:)',dr65_i2(2,:)',dr65_i3(2,:)',dr65_d1(2,:)',dr65_d2(2,:)',dr65_d3(2,:)'];
+
 graph(dr65_i1,r65_i1,1);
 graph(dr65_i2,r65_i2,2);
 graph(dr65_i3,r65_i3,3);
@@ -404,6 +518,13 @@ graph(dr65_d2,r65_d2,5);
 graph(dr65_d3,r65_d3,6);
 
 r65_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 70
 
@@ -427,6 +548,8 @@ r70_d3 = fit(dr70_d3,[0.027 35 0 1.1]);
 
 vals = [r70_i1 r70_i2 r70_i3 r70_d1 r70_d2 r70_d3];
 
+old_eles = [dr70_i1(2,:)',dr70_i2(2,:)',dr70_i3(2,:)',dr70_d1(2,:)',dr70_d2(2,:)',dr70_d3(2,:)'];
+
 graph(dr70_i1,r70_i1,1);
 graph(dr70_i2,r70_i2,2);
 graph(dr70_i3,r70_i3,3);
@@ -435,6 +558,13 @@ graph(dr70_d2,r70_d2,5);
 graph(dr70_d3,r70_d3,6);
 
 r70_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 75
 
@@ -458,6 +588,8 @@ r75_d3 = fit(dr75_d3,[0.027 36 0 1.1]);
 
 vals = [r75_i1 r75_i2 r75_i3 r75_d1 r75_d2 r75_d3];
 
+old_eles = [dr75_i1(2,:)',dr75_i2(2,:)',dr75_i3(2,:)',dr75_d1(2,:)',dr75_d2(2,:)',dr75_d3(2,:)'];
+
 graph(dr75_i1,r75_i1,1);
 graph(dr75_i2,r75_i2,2);
 graph(dr75_i3,r75_i3,3);
@@ -466,6 +598,13 @@ graph(dr75_d2,r75_d2,5);
 graph(dr75_d3,r75_d3,6);
 
 r75_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 80
 
@@ -489,6 +628,8 @@ r80_d3 = fit(dr80_d3,[0.027 36 0 1.1]);
 
 vals = [r80_i1 r80_i2 r80_i3 r80_d1 r80_d2 r80_d3];
 
+old_eles = [dr80_i1(2,:)',dr80_i2(2,:)',dr80_i3(2,:)',dr80_d1(2,:)',dr80_d2(2,:)',dr80_d3(2,:)'];
+
 graph(dr80_i1,r80_i1,1);
 graph(dr80_i2,r80_i2,2);
 graph(dr80_i3,r80_i3,3);
@@ -497,6 +638,13 @@ graph(dr80_d2,r80_d2,5);
 graph(dr80_d3,r80_d3,6);
 
 r80_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 85
 
@@ -520,6 +668,8 @@ r85_d3 = fit(dr85_d3,[0.027 36 0 1.1]);
 
 vals = [r85_i1 r85_i2 r85_i3 r85_d1 r85_d2 r85_d3];
 
+old_eles = [dr85_i1(2,:)',dr85_i2(2,:)',dr85_i3(2,:)',dr85_d1(2,:)',dr85_d2(2,:)',dr85_d3(2,:)'];
+
 graph(dr85_i1,r85_i1,1);
 graph(dr85_i2,r85_i2,2);
 graph(dr85_i3,r85_i3,3);
@@ -528,6 +678,13 @@ graph(dr85_d2,r85_d2,5);
 graph(dr85_d3,r85_d3,6);
 
 r85_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 90
 
@@ -551,6 +708,8 @@ r90_d3 = fit(dr90_d3,[0.027 36 0 1.1]);
 
 vals = [r90_i1 r90_i2 r90_i3 r90_d1 r90_d2 r90_d3];
 
+old_eles = [dr90_i1(2,:)',dr90_i2(2,:)',dr90_i3(2,:)',dr90_d1(2,:)',dr90_d2(2,:)',dr90_d3(2,:)'];
+
 graph(dr90_i1,r90_i1,1);
 graph(dr90_i2,r90_i2,2);
 graph(dr90_i3,r90_i3,3);
@@ -559,6 +718,13 @@ graph(dr90_d2,r90_d2,5);
 graph(dr90_d3,r90_d3,6);
 
 r90_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 95
 
@@ -582,6 +748,8 @@ r95_d3 = fit(dr95_d3,[0.027 36 0 1.1]);
 
 vals = [r95_i1 r95_i2 r95_i3 r95_d1 r95_d2 r95_d3];
 
+old_eles = [dr95_i1(2,:)',dr95_i2(2,:)',dr95_i3(2,:)',dr95_d1(2,:)',dr95_d2(2,:)',dr95_d3(2,:)'];
+
 graph(dr95_i1,r95_i1,1);
 graph(dr95_i2,r95_i2,2);
 graph(dr95_i3,r95_i3,3);
@@ -590,6 +758,13 @@ graph(dr95_d2,r95_d2,5);
 graph(dr95_d3,r95_d3,6);
 
 r95_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% 100
 
@@ -613,6 +788,8 @@ r100_d3 = fit(dr100_d3,[0.027 36 0 1.1]);
 
 vals = [r100_i1 r100_i2 r100_i3 r100_d1 r100_d2 r100_d3];
 
+old_eles = [dr100_i1(2,:)',dr100_i2(2,:)',dr100_i3(2,:)',dr100_d1(2,:)',dr100_d2(2,:)',dr100_d3(2,:)'];
+
 graph(dr100_i1,r100_i1,1);
 graph(dr100_i2,r100_i2,2);
 graph(dr100_i3,r100_i3,3);
@@ -621,6 +798,13 @@ graph(dr100_d2,r100_d2,5);
 graph(dr100_d3,r100_d3,6);
 
 r100_mag_shi = secondvals(vals(2,:),wn,zeta);
+
+% Random Uncertainty
+eles = linspace(0,4.995,1000);
+
+new_eles = fetch(eles,vals);
+
+standdev = dev(new_eles,old_eles);
 
 %% Functions
 
@@ -656,4 +840,26 @@ function d=secondvals(w,wn,zeta)
     d = zeros(6,2);
     d(:,1) = (1./sqrt(a.^2+b.^2))';
     d(:,2) = (-atan(a./b))';
+end
+
+% makes a matrix
+function e=fetch(x,func)
+    e = zeros(length(x),1);
+    for i=1:length(func)
+        e = [e,(func(1,i)*sin(func(2,i)*x+func(3,i))+func(4,i))'];
+    end
+    e = e(:,2:end);
+end
+
+function f=dev(fit,data)
+    a = size(fit);
+    a = a(2);
+    b = a(1);
+    e = zeros(1,a);
+    for i=1:a
+        for j=1:b
+            e(i) = e(i) + (fit(j,i) - data(j,i))^2;
+        end
+    end
+    f = 1.96*sqrt(e/(length(fit)-2));
 end
