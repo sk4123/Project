2a4380034f1e8f82bded839f9fbdfcff2c33da1a
sk4123 <66120293+sk4123@users.noreply.github.com>
2024-04-26 11:31:07 -0400

    I cannot make code run
    smh

--- dev.m
+++ dev.m
@@ -1,11 +1,10 @@
 % find the standard deviation of the fit
 function f=dev(fit,data)
-    a = size(fit);
-    a = a(2);
-    b = a(1);
+    a = length(fit);
     e = zeros(1,a);
-        for j=1:b
-            e(j) = e(j) + (data(1,j) - fit(1,j))^2;
+        for j=1:a
+            e(j+1) = e(j) + (data(1,j) - fit(1,j))^2;
         end
     f = 1.96*sqrt(e/(length(fit)-2));
+    f = f(2:end);
 end
